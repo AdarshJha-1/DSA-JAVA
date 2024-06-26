49c8fe454ac40fcc4aa52a60fa6975de5f50244d
Adarsh Jha <adarsh2005j@gmail.com>
2024-06-26 16:45:39 +0530

    Arrays | Q1

--- src/Arrays/buildArrayfromPermutation1920/BuildArrayFromPermutation.java
+++ src/Arrays/buildArrayfromPermutation1920/BuildArrayFromPermutation.java
@@ -12,7 +12,7 @@ public class BuildArrayFromPermutation {
 
     static int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < ans.length; i++){
             ans[i] = nums[nums[i]];
         }
         return ans;
